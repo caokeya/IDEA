15181d486463af9068b2d75a5bbec5862513d207
caokeya <240912721@qq.com>
2018-08-27 15:24:15 +0800

    Leetcode

--- com/company/ContainerWithMostWater.java
+++ com/company/ContainerWithMostWater.java
@@ -4,25 +4,25 @@ package com.company;
 
 public class ContainerWithMostWater {
 
-	class Solution {
-		public int maxArea(int[] height) {
-			int L = height.length, lo = 0, hi = L - 1;
-			int max = 0;
-			while (lo < hi) {
-				int loMax = height[lo], hiMax = height[hi];
+    class Solution {
+        public int maxArea(int[] height) {
+            int L = height.length, lo = 0, hi = L - 1;
+            int max = 0;
+            while (lo < hi) {
+                int loMax = height[lo], hiMax = height[hi];
 
-				int candidate = (hi - lo) * (loMax < hiMax ? loMax : hiMax);
-				max = candidate > max ? candidate : max;
+                int candidate = (hi - lo) * (loMax < hiMax ? loMax : hiMax);
+                max = candidate > max ? candidate : max;
 
-				if (height[lo] <= height[hi])
-					while (lo < hi && height[lo] <= loMax)
-						++lo;
-				else
-					while (hi > lo && height[hi] <= hiMax)
-						--hi;
-			}
-			return max;
-		}
-	}
+                if (height[lo] <= height[hi])
+                    while (lo < hi && height[lo] <= loMax)
+                        ++lo;
+                else
+                    while (hi > lo && height[hi] <= hiMax)
+                        --hi;
+            }
+            return max;
+        }
+    }
 
 }
